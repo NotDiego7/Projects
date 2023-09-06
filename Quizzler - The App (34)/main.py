1732a1844318fd627f547fef0c3046692dea6beb
NotDiego7 <Lopez.d7@outlook.com>
2023-09-05 23:09:27 -0600

    Progress

--- Quizzler - The App (34)/main.py
+++ Quizzler - The App (34)/main.py
@@ -1,19 +1,22 @@
+from ui import QuizUI
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from html import unescape
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
+    question_text = unescape(question["question"])
+    question_answer = unescape(question["correct_answer"])
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+front_end = QuizUI()
 
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Quizzler - The App (34)/ui.py
+++ Quizzler - The App (34)/ui.py
@@ -1,2 +1,47 @@
+from tkinter import *
+
+# --------------------------------- Constants -------------------------------- #
 THEME_COLOR = "#375362"
 
+class QuizUI():
+    def __init__(self):
+        # ----------------------------------- Setup ---------------------------------- #
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.configure(background=THEME_COLOR, padx=20, pady=20)
+
+        # -------------------------------- Load Images ------------------------------- #
+
+        self.false_img = PhotoImage(file="images/false.png")
+        self.true_img = PhotoImage(file="images/true.png")
+
+        # ---------------------- Create Canvas and Inject Images --------------------- #
+
+        self.canvas = Canvas(width= 300, height= 250, background= "gray", highlightthickness= 0)
+        self.false_button_image = self.canvas.create_image(100, 97, image= self.false_img)
+        self.true_button_image = self.canvas.create_image(300, 97, image= self.true_img)
+        self.displayed_query = self.canvas.create_text(250, 300, text="", font=("Times New Roman", 20, "italic"))
+
+        self.canvas.grid(column= 0, row= 0, columnspan= 2)
+        
+        # ---------------------------------- Buttons --------------------------------- #
+
+        self.true_button = Button(width=100, height=100, background=THEME_COLOR, highlightthickness=0, command=self.true_button_functionality, image=self.true_button_image, border=0)
+        self.true_button.grid(column=0, row=1)
+
+        self.false_button = Button(width=100, height=100, background=THEME_COLOR, highlightthickness=0, command=self.false_button_functionality, image=self.false_button_image, border=0)
+        self.false_button.grid(column=1, row=1)
+
+
+
+        self.window.mainloop()
+
+    
+
+    def true_button_functionality(self):
+        pass
+
+
+    def false_button_functionality(self):
+        pass
