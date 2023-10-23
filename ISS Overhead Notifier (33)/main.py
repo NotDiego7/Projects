1ceabbbfe763d606e38de0bafe095536652ad462
NotDiego7 <Lopez.d7@outlook.com>
2023-10-23 09:51:18 -0600

    Progress

--- ISS Overhead Notifier (33)/main.py
+++ ISS Overhead Notifier (33)/main.py
@@ -10,6 +10,7 @@ MY_LONG = -96.852115
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json()
+print(data)
 
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
