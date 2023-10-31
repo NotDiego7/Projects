cd9eae0ffe2fc6d7bd514ac4a3350c3c456cde71
NotDiego7 <Lopez.d7@outlook.com>
2023-10-30 21:24:41 -0600

    Progress

--- Web Scraping (45)/main.py
+++ Web Scraping (45)/main.py
@@ -3,9 +3,25 @@ from bs4 import BeautifulSoup
 with open(file= "website.html", mode= "r") as data:
     content = data.read()
 
+# ------------------------ Create BeautifulSoup Object ----------------------- #
 data = BeautifulSoup(markup= content, features= "html.parser")
-stuff = data.find_all(name= "h3")
-for i in stuff:
-    print(i.string)
 
-data.find(name= "h3")
+# -- Use find_all method to find all h3 and loop to get the strings in them -- #
+# stuff = data.find_all(name= "h3")
+# for i in stuff:
+#     print(i.string)
+
+# ------------------- Use find method to find the first h3 ------------------- #
+# data.find(name= "h3")
+
+# --------------- Getting the first anchor element and its href -------------- #
+# print(data.find(name= "a").get("href"))
+# print(data.a.get("href"))
+
+# --------- Get a specific elements href by using CSS-Selector syntax -------- #
+print(data.select(selector= "body p em strong a")[0].get("href"))
+
+"""
+The select() method is by far the best since you can use CSS-Selection just like in CSS
+"""
+
