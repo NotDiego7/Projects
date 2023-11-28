f84a9b7ef670b174a8ccaf795284a129fb73fd37
NotDiego7 <Lopez.d7@outlook.com>
2023-11-28 09:01:55 -0600

    Progress

--- Test/Debug.py
+++ Test/Debug.py
@@ -1,16 +1,17 @@
 import time
 
-# print(time.localtime().tm_sec)
+def log_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
 
-# secs = time.time()
-# print(time.localtime(secs))
+@log_execution_time
+def my_function(n):
+    for i in range(n):
+        print(i)
 
-def delay(function):
-    time.sleep(5.0)
-    return function()
-
-@delay
-def say_hello():
-    print('Hello, world.')
-
-say_hello()
+my_function(100)
