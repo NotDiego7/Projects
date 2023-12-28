0367062db1f74ac75a2bf2dea263e80d41e4249a
NotDiego7 <Lopez.d7@outlook.com>
2023-12-27 23:48:03 -0600

    Progress
    A WHOLE BUNCH OF PROGRESS

--- Site Template (Flask)/assets/js/ai.chat.js
+++ Site Template (Flask)/assets/js/ai.chat.js
@@ -1,23 +1,221 @@
-const sendMessageButton = document.getElementById('send-message-button');
+const uploadButton = document.getElementById('upload-image-button');
 
-sendMessageButton.addEventListener('click', function() {
-    const text = document.getElementById('message').value;
+uploadButton.addEventListener('click', () => {
+  const fileReader = new FileReader();
 
-    const xhr = new XMLHttpRequest();
+  // Function to handle the image data for display
+  fileReader.onload = (event) => {
+    const imageURL = event.target.result;
+    const imageElement = document.getElementById('image-preview');
+    imageElement.src = imageURL;
+  };
 
-    xhr.open('POST', 'https://notdiego7.pythonanywhere.com/'); // NEED TO CHANGE HERE
-    xhr.setRequestHeader('Content-Type', 'application/json');
+  // Trigger the file selection dialog
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'image/*';
+  input.click();
 
-    xhr.send(JSON.stringify({ text }));
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0]); // Read as data URL for display
 
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            const generatedText = JSON.parse(xhr.responseText).text;
+    // Upload image directly to Dropbox when the send button is clicked
+    const sendMessageButton = document.getElementById('send-message-button');
+    sendMessageButton.addEventListener('click', async () => {
+      const imageFile = input.files[0];
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(imageFile);
 
-            // Set the AI-generated text in the blockquote element.
-            document.querySelector('blockquote').innerText = generatedText;
-        } else {
-            document.querySelector('blockquote').innerText = 'Error';
+      reader.onloadend = async () => {
+        try {
+          // Upload image to Dropbox
+          const response = await fetch('https://content.dropboxapi.com/2/files/upload', {
+            method: 'POST',
+            headers: {
+              'Authorization': 'Bearer sl.BsgkCgkvqmoDyafZQlA-sKFrqZJElcrjVTCNSgFUzkTeoTjiGbKnwozlLXEod3JzpHOd9T23rHpnyL2L2q8Oia3eR9F43N0QS_hKaN0XEykeFxFfWlQR2kGVOUYJ46suZtM5FdxVguheC90GUB5N',  // Replace with your access token
+              'Content-Type': 'application/octet-stream',
+              'Dropbox-API-Arg': JSON.stringify({
+                path: '/image.jpg',
+                mode: 'overwrite'
+              })
+            },
+            body: reader.result
+          });
+
+
+          // Check if shared link already exists
+          const existingSharedLinkResponse = await fetch('https://api.dropboxapi.com/2/sharing/list_shared_links', {
+            method: 'POST',
+            headers: {
+              'Authorization': 'Bearer sl.BsgkCgkvqmoDyafZQlA-sKFrqZJElcrjVTCNSgFUzkTeoTjiGbKnwozlLXEod3JzpHOd9T23rHpnyL2L2q8Oia3eR9F43N0QS_hKaN0XEykeFxFfWlQR2kGVOUYJ46suZtM5FdxVguheC90GUB5N',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              path: '/image.jpg',
+              direct_only: true  // Only include direct shared links
+            })
+          });
+
+          const existingSharedLinkData = await existingSharedLinkResponse.json();
+
+          let dropboxPreviewPage;
+          if (existingSharedLinkData.links.length > 0) {
+            // Use existing shared link
+            dropboxPreviewPage = existingSharedLinkData.links[0].url;
+          } else {
+            // Create shared link if it doesn't exist
+            const createSharedLinkResponse = await fetch('https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings', {
+              method: 'POST',
+              headers: {
+                'Authorization': 'Bearer sl.BsgkCgkvqmoDyafZQlA-sKFrqZJElcrjVTCNSgFUzkTeoTjiGbKnwozlLXEod3JzpHOd9T23rHpnyL2L2q8Oia3eR9F43N0QS_hKaN0XEykeFxFfWlQR2kGVOUYJ46suZtM5FdxVguheC90GUB5N',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                path: '/image.jpg',
+                settings: {
+                  access: 'viewer',
+                  allow_download: true,
+                  audience: 'public',
+                  requested_visibility: 'public'
+                }
+              })
+            });
+
+            const dropboxSharedLinkResponse = await createSharedLinkResponse.json();
+            dropboxPreviewPage = dropboxSharedLinkResponse.url;
+          }
+
+
+          const text = document.getElementById('message').value;
+
+          // Call Python server-side proxy with CORS in-place to make requests to Gemini API
+          const pyProxyResponse = await fetch('https://notdiego7.pythonanywhere.com/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              prompt: text,
+              dropboxPreviewPage: dropboxPreviewPage
+            })
+          });
+          const ProxyResponse = await pyProxyResponse.json();
+          const generatedText = ProxyResponse.text;
+
+          document.querySelector('p#ai-body-text').innerText = generatedText;
+        } catch (error) {
+          console.error('Error:', error);
         }
-    };
+      };
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const uploadButton = document.getElementById('upload-image-button');
+
+// uploadButton.addEventListener('click', () => {
+//   const fileReader = new FileReader();
+
+//   // Function to handle the image data for display
+//   fileReader.onload = (event) => {
+//     const imageURL = event.target.result;
+//     const imageElement = document.getElementById('image-preview');
+//     imageElement.src = imageURL;
+//   };
+
+//   // Trigger the file selection dialog
+//   const input = document.createElement('input');
+//   input.type = 'file';
+//   input.accept = 'image/*';
+//   input.click();
+
+//   input.addEventListener('change', () => {
+//     fileReader.readAsDataURL(input.files[0]); // Read as data URL for display
+
+//     // Send image bytes as a Blob when the send button is clicked
+//     const sendMessageButton = document.getElementById('send-message-button');
+//     sendMessageButton.addEventListener('click', () => {
+//       const formData = new FormData();
+//       formData.append('text', document.getElementById('message').value);
+
+//       // Read image bytes directly
+//       const imageFile = input.files[0];
+//       const reader = new FileReader();
+//       reader.readAsArrayBuffer(imageFile);
+
+//       reader.onloadend = () => {
+//         formData.append('imageBytes', new Blob([reader.result])); // Append image bytes as a Blob
+
+//         fetch('https://notdiego7.pythonanywhere.com/', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'multipart/form-data'
+//           },
+//           body: formData
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//           document.querySelector('p#ai-body-text').innerText = data.text;
+//         })
+//         .catch(error => {
+//           console.error('Error sending image data:', error);
+//         });
+//       };
+//     });
+//   });
+// });
--- Site Template (Flask)/palm_interaction.py
+++ Site Template (Flask)/palm_interaction.py
@@ -1,113 +1,314 @@
 from flask import Flask, request, jsonify
 from google import generativeai
 from flask_cors import CORS
-from PIL import Image
+from bs4 import BeautifulSoup  # Import BeautifulSoup for parsing HTML
 from io import BytesIO
-import os, requests
+from PIL import Image
+import requests
 
-from datetime import datetime, timedelta
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.http import MediaFileUpload
-
-# ---------------------------------- Gemini ---------------------------------- #
+# -------------------------------- WebApp API -------------------------------- #
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST'])
 def generate_text_api():
-    text = request.json['text']
+    try:
+        request_data = request.get_json()
 
-    generatedText = generate_ai_text(text)
+        temporary_link = request_data['temporaryLink']
+        prompt = request_data['prompt']
 
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Headers': '*'
-    }
+        image = parse_preview_page_link(temporary_link)
 
+        generated_text = generate_ai_text(prompt, image)
 
-    return jsonify({'text': generatedText}), 200, headers
+        headers = {'Content-Type': 'application/json'}
+        return jsonify({'text': generated_text}), 200, headers
+
+    except Exception as e:
+        raise Exception(f"Error processing request: {e}")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# ----------------------- Main Function to be executed ----------------------- #
-def generate_ai_text(prompt):
-    # PALM_KEY = os.getenv('PALM_API_KEY')
-    # print(PALM_KEY)
-    """
-    NEED TO CHANGE THIS HARDCODE METHOD!!!!!!
-    """
-    # ---------------------------------------------------------------------------- #
-    PALM_KEY = "AIzaSyBfDMWMKteCfBPi4gXA2RYrV_G-ln7axj4"
-    # ---------------------------------------------------------------------------- #
-    """
-    NEED TO CHANGE THIS HARDCODE METHOD!!!!!!
-    """
+# ---------------------- Gemini Pro Vision Main Function --------------------- #
+def generate_ai_text(prompt, image_url):
+    PALM_KEY = "AIzaSyDh-DlmCVoUAHDT3GA5N20pRF668pWZnxk"
+    
     generativeai.configure(api_key= PALM_KEY)
     model = generativeai.GenerativeModel('gemini-pro-vision')
-    # TODO Here we basically need to say, if user uploaded image, then attach image and prompt, else just prompt
-    url = 'https://ia801404.us.archive.org/14/items/nhs-01-e-07/NHS01E01.jpg'
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "Cookie": "PHPSESSID=242a10656e1779cac246145de54684aa",
-    }
-    response = requests.get(url= url, headers= headers)
-    image_data = BytesIO(response.content)
-    image = Image.open(image_data)
-    response = model.generate_content([prompt, image])
-  
+    response = model.generate_content([prompt, image_url])
+
     return response.text
 
 
+def parse_preview_page_link(temporary_link) -> str:
+    image_bytes = requests.get(url= temporary_link).content
+    image_data = BytesIO(image_bytes)
+    image = Image.open(image_data)
+
+    return image
+
+    
 
 
 
 
-# ------------------------ Google Drive Functionality ------------------------ #
-# Define your Google Drive credentials
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-creds = flow.run_console_flow()
-drive_service = build('drive', 'v3', credentials=creds)
 
-# Function to upload an image to Google Drive
-def upload_image(image_path):
-  with open(image_path, 'rb') as file:
-    file_metadata = {
-      'name': os.path.basename(image_path),
-      'mimeType': 'image/jpeg'  # Adjust based on your image format
-    }
-    media = MediaFileUpload(file, mimetype='image/jpeg')
-    return drive_service.files().create(body=file_metadata, media_body=media).execute()
 
-# Upload the image
-uploaded_file = upload_image('path/to/your/image.jpg')
 
-# Schedule deletion for tomorrow
-delete_date = datetime.now() + timedelta(days=1)
 
-# Create a reminder to delete the file
-reminder = drive_service.files().insert(body={
-  'title': f'Delete {uploaded_file["name"]} on {delete_date.strftime("%Y-%m-%d")}',
-  'parents': ['your-trash-folder-id']  # Replace with your actual trash folder ID
-}).execute()
 
-# Set up a scheduled task to execute the following code at the delete date
-# (Replace with your preferred scheduling method)
-def delete_file():
-  drive_service.files().delete(uploaded_file['id']).execute()
-  drive_service.files().delete(reminder['id']).execute()
 
-# Replace this with your actual scheduling logic
-# ...
 
-# Print confirmation message
-print(f"Image uploaded to Google Drive: {uploaded_file['id']}")
-print(f"Scheduled for deletion on: {delete_date.strftime('%Y-%m-%d')}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------- JS -> Py -> dbx/Gemini -> JS ----------------------- #
+
+
+# from flask import Flask, request, jsonify
+# from google import generativeai
+# from flask_cors import CORS
+# from PIL import Image
+# from io import BytesIO
+# import os, requests, dropbox
+
+# # -------------------------------- WebApp API -------------------------------- #
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/', methods=['POST'])
+# def generate_text_api():
+#     try:
+#         print(f"1. This is the request size: {request.content_length}")
+
+#         image_bytes = request.get_data()
+
+#         image_bytes_io = BytesIO(image_bytes)
+
+#         # Save image temporarily and process
+#         with open('new_image.jpg', 'wb') as f:
+#             f.write(image_bytes_io)
+
+#         with open('/tmp/image.jpg', 'rb') as f:
+#             image_bytes_io = f.read()
+
+#         print(2)
+#         image_url = drop_box_logic(image_bytes_io)
+#         print(3)
+#         text = request.form.get('text')
+#         print(4)
+#         generated_text = generate_ai_text(text, image_url)
+#         print(5)
+
+#         headers = {'Content-Type': 'application/json'}
+#         return jsonify({'text': generated_text}), 200, headers
+
+#     except Exception as e:
+#         raise Exception(f"Error processing request: {e}")
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# # ---------------------- Gemini Pro Vision Main Function --------------------- #
+# def generate_ai_text(prompt, image_url= None):
+#     PALM_KEY = ""
+#     generativeai.configure(api_key=PALM_KEY)
+#     model = generativeai.GenerativeModel('gemini-pro-vision')
+
+#     # Conditionally use image if provided
+#     if image_url:
+#         headers = {
+#             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+#             "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+#             "Cookie": "PHPSESSID=242a10656e1779cac246145de54684aa",
+#         }
+#         response = requests.get(url= image_url, headers= headers)
+#         image_data = BytesIO(response.content)
+#         image = Image.open(image_data)
+#         response = model.generate_content([prompt, image])
+#     else:
+#         response = model.generate_content([prompt])
+
+#     return response.text
+
+
+
+
+
+
+# # -------------------------- Dropbox as Temp Storage ------------------------- #
+# def drop_box_logic(image_bytes_io):
+#     access_token = ""  # TODO: Replace with your Dropbox access token
+    
+#     try:
+#         print("Bout to try opening the image...")
+#         image = Image.open(image_bytes_io)
+#         print("well the damn image opened... Now we gonna check if it's all good by running the verify method on it...")
+#         image.verify()  # Check image integrity
+#         for i in range(11):
+#             print(f"Damn image opened successfully!")
+
+#         with dropbox.Dropbox(oauth2_access_token=access_token) as dbx:
+#             uploaded_file = dbx.files_upload(image_bytes_io.read(), "/image.jpg")  # Read from BytesIO for upload
+#             shared_link_url = dbx.sharing_create_shared_link_with_settings(
+#                 uploaded_file.path_lower
+#             ).url
+#             return shared_link_url
+
+#     except Exception as e:
+#         raise Exception(f"Error uploading image: {e}")
+
+
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1. Temporary File Storage:
+
+# Store the image temporarily on the server using request.files['imageData'].save('/tmp/image.jpg').
+# Access the image bytes from this temporary file using with open('/tmp/image.jpg', 'rb') as f: image_bytes = f.read().
+# Remember to delete the temporary file after processing: os.remove('/tmp/image.jpg'). 
+
+
+# # ------------------------ Google Drive Functionality ------------------------ #
+# from datetime import datetime, timedelta
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.http import MediaFileUpload
+
+# # Define your Google Drive credentials
+# SCOPES = ['https://www.googleapis.com/auth/drive.file']
+# flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+# creds = flow.run_console_flow()
+# drive_service = build('drive', 'v3', credentials=creds)
+
+# # Function to upload an image to Google Drive
+# def upload_image(image_path):
+#   with open(image_path, 'rb') as file:
+#     file_metadata = {
+#       'name': os.path.basename(image_path),
+#       'mimeType': 'image/jpeg'  # Adjust based on your image format
+#     }
+#     media = MediaFileUpload(file, mimetype='image/jpeg')
+#     return drive_service.files().create(body=file_metadata, media_body=media).execute()
+
+# # Upload the image
+# uploaded_file = upload_image('path/to/your/image.jpg')
+
+# # Schedule deletion for tomorrow
+# delete_date = datetime.now() + timedelta(days=1)
+
+# # Create a reminder to delete the file
+# reminder = drive_service.files().insert(body={
+#   'title': f'Delete {uploaded_file["name"]} on {delete_date.strftime("%Y-%m-%d")}',
+#   'parents': ['your-trash-folder-id']  # Replace with your actual trash folder ID
+# }).execute()
+
+# # Set up a scheduled task to execute the following code at the delete date
+# # (Replace with your preferred scheduling method)
+# def delete_file():
+#   drive_service.files().delete(uploaded_file['id']).execute()
+#   drive_service.files().delete(reminder['id']).execute()
+
+# # Replace this with your actual scheduling logic
+# # ...
+
+# # Print confirmation message
+# print(f"Image uploaded to Google Drive: {uploaded_file['id']}")
+# print(f"Scheduled for deletion on: {delete_date.strftime('%Y-%m-%d')}")
 
 
 
@@ -126,8 +327,6 @@ print(f"Scheduled for deletion on: {delete_date.strftime('%Y-%m-%d')}")
 
 
 # ----------------------------- Chat Based System ---------------------------- #
-
-
 # from flask import Flask, request, jsonify
 # from google import generativeai
 # from flask_cors import CORS
@@ -186,3 +385,35 @@ print(f"Scheduled for deletion on: {delete_date.strftime('%Y-%m-%d')}")
 #         generated_text = "No response candidates were returned by the Google API."
 
 #     return generated_text
+        
+
+
+
+
+
+
+
+
+
+
+# ------------------------ Get access_token via OAuth1 ----------------------- #
+# import dropbox
+# from dropbox import DropboxOAuth2FlowNoRedirect
+
+# APP_KEY = "d5jldlgvwbaop3f"
+# APP_SECRET = "3xek9a87iujkkal"
+
+# auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+
+# authorize_url = auth_flow.start()
+# print("1. Go to: " + authorize_url)
+# print("2. Click \"Allow\" (you might have to log in first).")
+# print("3. Copy the authorization code.")
+# auth_code = input("Enter the authorization code here: ").strip()
+
+# try:
+#     oauth_result = auth_flow.finish(auth_code)
+# except Exception as e:
+#     print('Error: %s' % (e,))
+#     exit(1)
+# ---------------------------------------------------------------------------- #
